6e42a212085e01c40b77d8af2fc437f77525403c
middaywords <xkjac04452017@sjtu.edu.cn>
2020-10-26 23:59:33 +0800

    Add js-sql-parser.

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ Parse, transform sql scripts, present them in web.
 #### ToDo
 
 - [x] Use antlr to parse sql scripts
+
+- [ ] Use Js-sql-parser to parse sql scripts
+
+  * Failed to parse `@` in sql script.
+
+    <img src="assets/image-20201026235823002.png" alt="image-20201026235823002" style="zoom:50%;" />
+
 - [ ] Process the parsed results
+
 - [ ] Present the results in web
 
---
+++ test-scripts/sql-script2.sql
@@ -0,0 +1,34 @@
+SELECT
+    mspa.prod_num AS prod_num,
+    mspa_qty AS mspa_qty,
+    isnull(px_mdve, 0.0) AS px_move,
+    mspa.mspa_qcy - isnull(px.px_move, 0.0) AS diff
+FROM
+(
+SELECT 
+    prod_num,
+    sum(dr_qty - cr_qty) AS mspa_qty
+FROM
+    IL_Cash_3
+WHERE pnl_date = @pnl_date
+GROUP BY prod_num
+) AS mspa
+LEFT JOIN
+(
+SELECT 
+    prod_num,
+    sum(CASE 
+            WHEN pnl_date = @pnl_date 
+            then closing_position 
+            ELSE -1 * (closing_position) 
+            END 
+    ) AS px_move 
+FROM IL_Cash 
+WHERE pnl_date IN (
+                @pnl_date,
+                @prev me_date
+            )
+AND prod type <> 'Monetary Claim' 
+GROUP BY prod_num
+) px 
+ON mspa.prod_num = px.prod_oum
